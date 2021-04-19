329966e1fc0a472df76ae1f83a040709a0eb7c29
Jon Lane <admiralorbiter@gmail.com>
2021-04-18 21:04:10 -0500

    Added content and backend

---
+++ js/login-page.js
@@ -0,0 +1,20 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "test" && password === "test") {
+        var sessionTimeout = 1; //hours
+        var loginDuration = new Date();
+        loginDuration.setTime(loginDuration.getTime()+(sessionTimeout*60*60*1000));
+        document.cookie = "MathRelaySession=Valid; "+loginDuration.toGMTString()+"; path=/";
+        alert("You have successfully logged in.");
+        location=("hidden.html");
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})

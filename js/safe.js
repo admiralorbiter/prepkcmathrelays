b1e0c4099c94872bb7a9a6b9c68979f113871a07
Jonathan Lane <admiralorbiter@gmail.com>
2022-04-28 10:46:44 -0500

    Fixed safe

--- js/safe.js
+++ js/safe.js
@@ -54,8 +54,8 @@ test.onclick = function(){
   
   lamp.style.animation = "lamp-validation 500ms linear infinite"
 
-  secret = "http://pathwaystotechnology.net/assets/"+pass.value+".jpg?raw=true"
-  common = "http://pathwaystotechnology.net/assets/"+pass.value+".jpg?raw=true"
+  secret = "http://prepkcmathrelays.org/assets/"+pass.value+".jpg?raw=true"
+  common = "http://prepkcmathrelays.org/assets/"+pass.value+".jpg?raw=true"
   
   loadstate = 1
   SetContents(secret)
